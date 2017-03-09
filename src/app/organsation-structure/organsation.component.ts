8826df4fe06a7746e75bfe6d1b2119f42abf5ad4
ronscoder <rons.ubm@gmail.com>
2017-03-09 15:35:52 +0530

    replace org struc with chart

--- src/app/organsation-structure/organsation.component.ts
+++ src/app/organsation-structure/organsation.component.ts
@@ -1,14 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 import { Http } from '@angular/http';
 import { DataServiceService } from '../data-service.service';
+
+declare var google: any;
 @Component({
   selector: 'app-organsation-structure',
   templateUrl: './organsation-structure.component.html',
   styleUrls: ['./organsation-structure.component.css']
 })
 export class OrgansationStructureComponent implements OnInit {
-  constructor() { }
+  constructor(private http: Http) { }
   ngOnInit() {
+    this.http.get('./app/data/orgchart.json').subscribe(
+      res => {
+        google.charts.load('current', { packages: ["orgchart"] });
+        google.charts.setOnLoadCallback(() => {
+          var data = new google.visualization.DataTable();
+          data.addColumn('string', 'Name');
+          data.addColumn('string', 'Manager');
+          data.addColumn('string', 'ToolTip');
+
+          // For each orgchart box, provide the name, manager, and tooltip to show.
+
+          data.addRows(
+            res.json()
+          );
+
+          // Create the chart.
+          var chart = new google.visualization.OrgChart(document.getElementById('org_struct'));
+          // Draw the chart, setting the allowHtml option to true for the tooltips.
+          chart.draw(data, { allowHtml: true });
+        });
+      }
+    )
+
+
   }
 }
 
@@ -41,5 +67,6 @@ export class OrganProfile implements OnInit {
     })
     // this.boardMembers = StaticData.boardMembers.data;
     // this.memberHeading = StaticData.boardMembers.heading;
+
   }
 }
